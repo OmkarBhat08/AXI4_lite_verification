6b1f91b5750fef9743c942a9bb32febfcbff5444
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-02-20 14:57:09 +0530

    Update axi4_seq_item.sv

--- design_3_krishna/src./axi4_seq_item.sv
+++ design_3_krishna/src./axi4_seq_item.sv
@@ -1,66 +1,69 @@
 class axi4_seq_item extends uvm_sequence_item;
 
-  // ================= AXI WRITE ADDRESS CHANNEL =================
-  rand bit [`ADDR_WIDTH:0]   S_AXI_AWADDR;
-  rand bit [2:0]   S_AXI_AWPROT;
-  rand bit         S_AXI_AWVALID;
-  logic            S_AXI_AWREADY;
+  // ================= WRITE ADDRESS CHANNEL =================
+  rand bit [`ADDR_WIDTH-1:0]   AWADDR;
+  rand bit [2:0]               AWPROT;
+  rand bit                     AWVALID;
+  logic                        AWREADY;
 
-  // ================= AXI WRITE DATA CHANNEL =================
-  rand bit [`DATA_WIDTH:0]  S_AXI_WDATA;
-  rand bit [3:0]   S_AXI_WSTRB;
-  rand bit         S_AXI_WVALID;
-  logic            S_AXI_WREADY;
+  // ================= WRITE DATA CHANNEL =================
+  rand bit [`DATA_WIDTH-1:0]   WDATA;
+  rand bit [3:0]               WSTRB;
+  rand bit                     WVALID;
+  logic                        WREADY;
 
-  // ================= AXI WRITE RESPONSE CHANNEL =================
-  logic [1:0]      S_AXI_BRESP;
-  logic            S_AXI_BVALID;
-  rand bit         S_AXI_BREADY;
+  // ================= WRITE RESPONSE CHANNEL =================
+  logic [1:0]                  BRESP;
+  logic                        BVALID;
+  rand bit                     BREADY;
 
-  // ================= AXI READ ADDRESS CHANNEL =================
-  rand bit [`ADDR_WIDTH:0]   S_AXI_ARADDR;
-  rand bit [2:0]   S_AXI_ARPROT;
-  rand bit         S_AXI_ARVALID;
-  logic            S_AXI_ARREADY;
+  // ================= READ ADDRESS CHANNEL =================
+  rand bit [`ADDR_WIDTH-1:0]   ARADDR;
+  rand bit [2:0]               ARPROT;
+  rand bit                     ARVALID;
+  logic                        ARREADY;
 
-  // ================= AXI READ DATA CHANNEL =================
-  logic [`DATA_WIDTH:0]     S_AXI_RDATA;
-  logic [1:0]      S_AXI_RRESP;
-  logic            S_AXI_RVALID;
-  rand bit         S_AXI_RREADY;
+  // ================= READ DATA CHANNEL =================
+  logic [`DATA_WIDTH-1:0]      RDATA;
+  logic [1:0]                  RRESP;
+  logic                        RVALID;
+  rand bit                     RREADY;
 
-  // ================= EXTERNAL OUTPUTS =================
-  logic [3:0]      LED;
-  logic [6:0]      SEG_CATHODE;
-  logic [3:0]      SEG_ANODE;
-  logic            IRQ_OUT;
+  // ================= EXTERNAL INTERRUPT =================
+  rand bit                     EXT_IRQ_IN;
+
+  // ================= OUTPUTS =================
+  logic [3:0]                  LED;
+  logic [6:0]                  SEG_CATHODE;
+  logic [3:0]                  SEG_ANODE;
+  logic                        IRQ_OUT;
 
 
-  `uvm_object_utils_begin(axi_lite_slave_seq_item)
+  `uvm_object_utils_begin(axi4_seq_item)
 
-    `uvm_field_int(S_AXI_AWADDR, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_AWPROT, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_AWVALID, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_AWREADY, UVM_ALL_ON)
+    `uvm_field_int(AWADDR, UVM_ALL_ON)
+    `uvm_field_int(AWPROT, UVM_ALL_ON)
+    `uvm_field_int(AWVALID, UVM_ALL_ON)
+    `uvm_field_int(AWREADY, UVM_ALL_ON)
 
-    `uvm_field_int(S_AXI_WDATA, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_WSTRB, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_WVALID, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_WREADY, UVM_ALL_ON)
+    `uvm_field_int(WDATA, UVM_ALL_ON)
+    `uvm_field_int(WSTRB, UVM_ALL_ON)
+    `uvm_field_int(WVALID, UVM_ALL_ON)
+    `uvm_field_int(WREADY, UVM_ALL_ON)
 
-    `uvm_field_int(S_AXI_BRESP, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_BVALID, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_BREADY, UVM_ALL_ON)
+    `uvm_field_int(BRESP, UVM_ALL_ON)
+    `uvm_field_int(BVALID, UVM_ALL_ON)
+    `uvm_field_int(BREADY, UVM_ALL_ON)
 
-    `uvm_field_int(S_AXI_ARADDR, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_ARPROT, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_ARVALID, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_ARREADY, UVM_ALL_ON)
+    `uvm_field_int(ARADDR, UVM_ALL_ON)
+    `uvm_field_int(ARPROT, UVM_ALL_ON)
+    `uvm_field_int(ARVALID, UVM_ALL_ON)
+    `uvm_field_int(ARREADY, UVM_ALL_ON)
 
-    `uvm_field_int(S_AXI_RDATA, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_RRESP, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_RVALID, UVM_ALL_ON)
-    `uvm_field_int(S_AXI_RREADY, UVM_ALL_ON)
+    `uvm_field_int(RDATA, UVM_ALL_ON)
+    `uvm_field_int(RRESP, UVM_ALL_ON)
+    `uvm_field_int(RVALID, UVM_ALL_ON)
+    `uvm_field_int(RREADY, UVM_ALL_ON)
 
     `uvm_field_int(EXT_IRQ_IN, UVM_ALL_ON)
 
