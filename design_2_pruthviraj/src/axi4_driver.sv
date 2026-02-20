e9e582687761f13c21d42205ac9f5d96a6a37a3d
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-02-20 12:51:20 +0530

    Update axi4_driver.sv

--- design_2_pruthviraj/src/axi4_driver.sv
+++ design_2_pruthviraj/src/axi4_driver.sv
@@ -1,30 +1,44 @@
-class axi4_driver extends uvm_driver#(axi4_seq_item);
-    `uvm_component_utils(axi4_driver)
-    virtual axi4_interface vif;
+class axi4_driver extends uvm_driver #(axi4_seq_item);
 
-    function new(string name = "axi4_driver", uvm_component parent = null);
-        super.new(name, parent);
-    endfunction: new
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-        if( !(uvm_config_db#(virtual axi4_interface)::get(this, "", "vif", vif) )
-            `uvm_fatal("DRIVER", "NO VIRTUAL INTERFACE IN DRIVER")
-    endfunction: build_phase
+  `uvm_component_utils(axi4_driver)
 
-    task run_phase(uvm_phase phase);
-        repeat(2)@(vif.drv_cb);
-        forever begin
-            seq_item_port.get_next_item(req);
-            drive_task();
-            seq_item_port.item_done();
-        end
-    endtask: run_phase
-    task drive_task();
+  virtual inf vif;
 
-        `uvm_info(" DRV ", $sformatf(" ", ), UVM_LOW)
-        repeat( )@(vif.drv_cb);
-    endtask: drive_task
+//-----------------------new constructor---------------------------//
+  function new(string name = "axi4_driver",uvm_component parent = null);
+    super.new(name, parent);
+  endfunction : new
 
-endclass: axi4_driver
+//---------------------build phase------------------------------//
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
 
- 
+      if (!(uvm_config_db #(virtual inf)::get(this, "", "vif", vif)))
+      `uvm_fatal("DRIVER", "NO VIRTUAL INTERFACE IN DRIVER")
+
+  endfunction 
+
+//---------------------run phase-----------------------------//
+  task run_phase(uvm_phase phase);
+
+    repeat (2) @(vif.drv_cb);
+
+    forever begin
+      seq_item_port.get_next_item(req);
+      drive_task();
+      seq_item_port.item_done();
+    end
+
+  endtask 
+
+//----------------------task drive-------------------------//
+  task drive_task();
+
+    `uvm_info("DRV", $sformatf("Driving transaction"), UVM_LOW)
+
+    repeat (1) @(vif.drv_cb);
+
+  endtask 
+
+
+endclass 
