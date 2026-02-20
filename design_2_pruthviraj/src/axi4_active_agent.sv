719a164380bbaea4fb7665f2ab0d79562c39a175
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-02-20 12:32:31 +0530

    Update axi4_active_agent.sv

--- design_2_pruthviraj/src/axi4_active_agent.sv
+++ design_2_pruthviraj/src/axi4_active_agent.sv
@@ -1,1 +1,17 @@
+class axi4_passive_agent extends uvm_agent;
+    `uvm_component_utils(axi4_passive_agent)
+    axi4_passive_monitor p_mon_h;
+
+    uvm_active_passive_enum is_active;
+
+    function new(string name = "axi4_passive_agent", uvm_component parent = null);
+        super.new(name, parent);
+    endfunction: new
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        p_mon_h = axi4_passive_monitor::type_id::create("p_mon_h", this);
+    endfunction: build_phase
+
+endclass
 
