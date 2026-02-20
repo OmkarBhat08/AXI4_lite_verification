3927e1a1bd92811779afe3db6c798aa381250cad
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-02-20 15:02:23 +0530

    Update axi4_interface.sv

--- design_3_krishna/src./axi4_interface.sv
+++ design_3_krishna/src./axi4_interface.sv
@@ -1,85 +1,84 @@
-interface inf(input bit ACLK,input bit ARESETN);
+`include "define.sv"
+
+interface inf (input bit ACLK,input bit ARESETn);
 
   // ================= WRITE ADDRESS CHANNEL =================
-  bit   [`ADDR_WIDTH-1:0]  S_AXI_AWADDR;
-  bit   [2:0]              S_AXI_AWPROT;
-  bit                      S_AXI_AWVALID;
-  logic                    S_AXI_AWREADY;
+  bit   [`ADDR_WIDTH-1:0]  AWADDR;
+  bit   [2:0]              AWPROT;
+  bit                      AWVALID;
+  logic                    AWREADY;
 
   // ================= WRITE DATA CHANNEL =================
-  bit   [`DATA_WIDTH-1:0]  S_AXI_WDATA;
-  bit   [3:0]              S_AXI_WSTRB;
-  bit                      S_AXI_WVALID;
-  logic                    S_AXI_WREADY;
+  bit   [`DATA_WIDTH-1:0]  WDATA;
+  bit   [3:0]              WSTRB;
+  bit                      WVALID;
+  logic                    WREADY;
 
   // ================= WRITE RESPONSE CHANNEL =================
-  logic [1:0]              S_AXI_BRESP;
-  logic                    S_AXI_BVALID;
-  bit                      S_AXI_BREADY;
+  logic [1:0]              BRESP;
+  logic                    BVALID;
+  bit                      BREADY;
 
   // ================= READ ADDRESS CHANNEL =================
-  bit   [`ADDR_WIDTH-1:0]   S_AXI_ARADDR;
-  bit   [2:0]              S_AXI_ARPROT;
-  bit                      S_AXI_ARVALID;
-  logic                    S_AXI_ARREADY;
+  bit   [`ADDR_WIDTH-1:0]  ARADDR;
+  bit   [2:0]              ARPROT;
+  bit                      ARVALID;
+  logic                    ARREADY;
 
   // ================= READ DATA CHANNEL =================
-  logic [`DATA_WIDTH-1:0]   S_AXI_RDATA;
-  logic [1:0]              S_AXI_RRESP;
-  logic                    S_AXI_RVALID;
-  bit                      S_AXI_RREADY;
+  logic [`DATA_WIDTH-1:0]  RDATA;
+  logic [1:0]              RRESP;
+  logic                    RVALID;
+  bit                      RREADY;
 
-  // ================= EXTERNAL SIGNALS =================
+  // ================= EXTERNAL INTERRUPT =================
   bit                      EXT_IRQ_IN;
 
+  // ================= OUTPUTS =================
   logic [3:0]              LED;
   logic [6:0]              SEG_CATHODE;
   logic [3:0]              SEG_ANODE;
   logic                    IRQ_OUT;
 
 
-  // =========================================================
-  // DRIVER CLOCKING BLOCK
-  // =========================================================
+  // ================= DRIVER CLOCKING BLOCK =================
   clocking drv_cb @(posedge ACLK);
+
     default input #0 output #0;
-    
-    output S_AXI_AWADDR, S_AXI_AWPROT, S_AXI_AWVALID;
-    input  S_AXI_AWREADY;
 
-    output S_AXI_WDATA, S_AXI_WSTRB, S_AXI_WVALID;
-    input  S_AXI_WREADY;
+    output AWADDR, AWPROT, AWVALID;
+    input  AWREADY;
 
-    input  S_AXI_BRESP, S_AXI_BVALID;
-    output S_AXI_BREADY;
+    output WDATA, WSTRB, WVALID;
+    input  WREADY;
 
-    output S_AXI_ARADDR, S_AXI_ARPROT, S_AXI_ARVALID;
-    input  S_AXI_ARREADY;
+    input  BRESP, BVALID;
+    output BREADY;
 
-    input  S_AXI_RDATA, S_AXI_RRESP, S_AXI_RVALID;
-    output S_AXI_RREADY;
+    output ARADDR, ARPROT, ARVALID;
+    input  ARREADY;
+
+    input  RDATA, RRESP, RVALID;
+    output RREADY;
 
     output EXT_IRQ_IN;
-
-    input  LED, SEG_CATHODE, SEG_ANODE, IRQ_OUT;
 
   endclocking
 
 
-  // =========================================================
-  // MONITOR CLOCKING BLOCK
-  // =========================================================
+  // ================= MONITOR CLOCKING BLOCK =================
   clocking mon_cb @(posedge ACLK);
+    
     default input #0 output #0;
-    input S_AXI_AWADDR, S_AXI_AWPROT, S_AXI_AWVALID, S_AXI_AWREADY;
-    input S_AXI_WDATA, S_AXI_WSTRB, S_AXI_WVALID, S_AXI_WREADY;
-    input S_AXI_BRESP, S_AXI_BVALID, S_AXI_BREADY;
+    
+    input AWADDR, AWPROT, AWVALID, AWREADY;
+    input WDATA, WSTRB, WVALID, WREADY;
+    input BRESP, BVALID, BREADY;
 
-    input S_AXI_ARADDR, S_AXI_ARPROT, S_AXI_ARVALID, S_AXI_ARREADY;
-    input S_AXI_RDATA, S_AXI_RRESP, S_AXI_RVALID, S_AXI_RREADY;
+    input ARADDR, ARPROT, ARVALID, ARREADY;
+    input RDATA, RRESP, RVALID, RREADY;
 
     input EXT_IRQ_IN;
-
     input LED, SEG_CATHODE, SEG_ANODE, IRQ_OUT;
 
   endclocking
