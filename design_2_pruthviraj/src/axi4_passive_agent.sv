cdaea763fa1a504f9f204c52fdc51816973ceaa3
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-02-20 12:49:25 +0530

    Update axi4_passive_agent.sv

--- design_2_pruthviraj/src/axi4_passive_agent.sv
+++ design_2_pruthviraj/src/axi4_passive_agent.sv
@@ -1,29 +1,20 @@
-// Active agent
+class axi4_passive_agent extends uvm_agent;
 
-class axi4_active_agent extends uvm_agent;
-    `uvm_component_utils(axi4_active_agent)
-    axi4_driver drv_h;
-    axi4_active_monitor a_mon_h;
-    axi4_sequencer sqr_h;
+  `uvm_component_utils(axi4_passive_agent)
 
-    uvm_active_passive_enum is_active;
+  axi4_passive_monitor p_mon_h;
 
-    function new(string name = "axi4_active_agent", uvm_component parent = null);
-        super.new(name, parent);
-    endfunction: new
+//---------------------------new constructor----------------------//
+  function new(string name = "axi4_passive_agent",uvm_component parent = null);
+    super.new(name, parent);
+  endfunction 
 
-    function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
-        drv_h = axi4_driver::type_id::create("drv_h", this);
-        sqr_h = axi4_sequencer::type_id::create("sqr_h", this);
-        a_mon_h = axi4_active_monitor::type_id::create("a_mon_h", this);
-    endfunction: build_phase
-    function void connect_phase(uvm_phase phase);
-        super.connect_phase(phase);
-        drv_h.seq_item_port.connect(sqr_h.seq_item.export);
-    endfunction: connect_phase
+//-----------------------build phase-------------------------//
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    p_mon_h = axi4_passive_monitor::type_id::create("p_mon_h", this);
+
+  endfunction 
 
 endclass
-
-
- 
