1541f66dff676bdc080c4d53f0d2080a424be276
Raj-HR-21 <rajendrahr21@gmail.com>
2026-02-25 15:53:55 +0530

    updated seq and test

--- design_2_prithviraj/src/axi4_test.sv
+++ design_2_prithviraj/src/axi4_test.sv
@@ -1,5 +1,4 @@
 class axi4_base_test extends uvm_test;
-
   `uvm_component_utils(axi4_base_test)
 
   axi4_base_seq     base_seq;
@@ -9,22 +8,260 @@ class axi4_base_test extends uvm_test;
     super.new(name, parent);
   endfunction
 
-
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
     env = axi4_environment::type_id::create("env", this);
   endfunction 
 
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    base_seq = axi4_base_seq::type_id::create("base_seq");
+    base_seq.start(env.sqr);
+    phase.drop_objection(this);
+  endtask
+
+endclass
+
+// -----------------------------------------------------------------
+class simple_write_test extends axi4_base_test;
+  `uvm_component_utils(simple_write_test)
+
+  simple_write seq;
+
+  function new(string name = "simple_write_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
 
   virtual task run_phase(uvm_phase phase);
     super.run_phase(phase);
 
     phase.raise_objection(this);
-
-    base_seq = axi4_base_seq::type_id::create("base_seq");
-    base_seq.start(env.seqr);
-
+    seq = simple_write::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
     phase.drop_objection(this);
   endtask
-
 endclass
+
+// -----------------------------------------------------------------
+class simple_read_test extends axi4_base_test;
+  `uvm_component_utils(simple_read_test)
+
+  simple_read seq;
+
+  function new(string name = "simple_read_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = simple_read_test::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class read_followed_by_write_test extends axi4_base_test;
+  `uvm_component_utils(read_followed_by_write_test)
+
+  read_followed_by_write seq;
+
+  function new(string name = "read_followed_by_write_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = read_followed_by_write::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class parallel_read_write_test extends axi4_base_test;
+  `uvm_component_utils(parallel_read_write_test)
+
+  parallel_read_write seq;
+
+  function new(string name = "parallel_read_write_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = parallel_read_write::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class data_before_addr_test extends axi4_base_test;
+  `uvm_component_utils(data_before_addr_test)
+
+  data_before_addr seq;
+
+  function new(string name = "data_before_addr_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = data_before_addr::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class addr_before_data_test extends axi4_base_test;
+  `uvm_component_utils(addr_before_data_test)
+
+  addr_before_data seq;
+
+  function new(string name = "addr_before_data_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = addr_before_data::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class data_with_addr_test extends axi4_base_test;
+  `uvm_component_utils(data_with_addr_test)
+
+  data_with_addr seq;
+
+  function new(string name = "data_with_addr_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = data_with_addr::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class continuous_write_test extends axi4_base_test;
+  `uvm_component_utils(continuous_write_test)
+
+  continuous_write seq;
+
+  function new(string name = "continuous_write_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = continuous_write::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class write_strobe_select_1_test extends axi4_base_test;
+  `uvm_component_utils(write_strobe_select_1_test)
+
+  write_strobe_select_1 seq;
+
+  function new(string name = "write_strobe_select_1_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = write_strobe_select_1::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class write_strobe_select_2_test extends axi4_base_test;
+  `uvm_component_utils(write_strobe_select_2_test)
+
+  write_strobe_select_2 seq;
+
+  function new(string name = "", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  virtual task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = write_strobe_select_2::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class invalid_addr_test extends axi4_base_test;
+  `uvm_component_utils(invalid_addr_test)
+
+  invalid_addr seq;
+
+  function new(string name = "invalid_addr", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    super.run_phase(phase);
+
+    phase.raise_objection(this);
+    seq = invalid_addr::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+class irq_test extends axi4_base_test;
+  `uvm_component_utils(irq_test)
+
+  irq_seq_1 seq;
+
+  function new(string name = "irq_test", uvm_component parent = null);
+    super.new(name, parent);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    super.run_phase(this);
+
+    phase.raise_objection(this);
+    seq = irq_seq_1::type_id::create("seq");
+    seq.start(env.act_agent.seqr);
+    phase.drop_objection(this);
+  endtask
+endclass
+
+
--- design_3_krishna/src/axi4_sequence.sv
+++ design_3_krishna/src/axi4_sequence.sv
@@ -1,19 +1,285 @@
-class axi4_base_seq extends uvm_sequence #(axi4_seq_item);
-  `uvm_object_utils(axi4_base_seq)
+class base_seq extends uvm_sequence #(axi4_seq_item);
+  `uvm_object_utils(base_seq)
   
-  function new(string name="axi4_base_seq"); 
+  function new(string name="base_seq"); 
     super.new(name); 
   endfunction
   
   task body();
     axi4_seq_item req;
-    repeat (100) 
-      begin
+    `uvm_info(get_type_name(), " ------ Base Sequence ------ ", UVM_NONE)
+    repeat (100) begin
       req = axi4_seq_item::type_id::create("req");
       start_item(req);
       req.randomize();
       finish_item(req);
     end
   endtask
-  
 endclass
+
+// -----------------------------------------------------------------
+// Simple write with strobe = 'b1111
+class simple_write extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(simple_write)
+
+  function new(string name = "simple_write");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Simple write with strobe = 'b1111 ------ ", UVM_NONE)
+    repeat(5) begin
+      req = axi4_seq_item::type_id::create("req");
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WVALID == 1; req.WSTRB == 4'b1111; req.BREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Simple read
+class simple_read extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(simple_read)
+
+  function new(string name = "simple_read");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Simple read ------ ", UVM_NONE)
+    repeat(5) begin
+      req = axi4_seq_item::type_id::create("req");
+      `uvm_do_with(req, {req.ARADDR inside {0, 4, 8}; req.ARVALID == 1; req.RREADY == 1;})
+
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Read followed by Write
+class read_followed_by_write extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(read_followed_by_write)
+  int prev_addr;
+
+  function new(string name = "read_followed_by_write");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Read followed by Write ------ ", UVM_NONE)
+    repeat(10) begin
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WSTRB == 4'b1111; req.WVALID == 1; req.BREADY == 1;})
+      prev_addr = req.AWADDR;
+      `uvm_do_with(req, {req.ARADDR == prev_addr; req.ARVALID == 1; req.RREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Parallel Read and Write
+class parallel_read_write extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(parallel_read_write)
+
+  function new(string name = "parallel_read_write");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Parallel Read and Write ------ ", UVM_NONE)
+    repeat(10) begin
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WSTRB == 4'b1111; req.WVALID == 1; req.ARADDR inside {0,4,8}; req.ARVALID == 1; req.BREADY == 1; req.RREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Write data before address
+class data_before_addr extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(data_before_addr)
+
+  function new(string name = "data_before_addr");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Write data before address ------ ", UVM_NONE)
+    repeat(10) begin
+      `uvm_do_with(req, {req.WVALID == 1; req.WSTRB == 4'b1111;})
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1;})
+      `uvm_do_with(req, {req.BREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Write address before data together
+class addr_before_data extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(addr_before_data)
+
+  function new(string name = "addr_before_data");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Write address before data together ------ ", UVM_NONE)
+    repeat(10) begin
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1;})
+      `uvm_do_with(req, {req.WVALID == 1; req.WSTRB == 4'b1111;})
+      `uvm_do_with(req, {req.BREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Write address and data together
+class data_with_addr extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(data_with_addr)
+
+  function new(string name = "data_with_addr");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Write address and data together ------ ", UVM_NONE)
+    repeat(10) begin
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WVALID == 1; req.WSTRB == 4'b1111;})
+      `uvm_do_with(req, {req.BREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Back to back write to same address and read
+class continuous_write extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(continuous_write)
+  int prev_addr, prev_data;
+
+  function new(string name = "continuous_write");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Back to back write to same address and read ------ ", UVM_NONE)
+    prev_addr = 0;
+    repeat(10) begin
+      `uvm_do_with(req, {req.AWADDR != prev_addr; req.AWVALID == 1; req.WSTRB == 4'b1111; req.WVALID == 1; req.BREADY == 1;})
+      prev_addr = req.AWADDR;
+      prev_data = req.WDATA;
+
+      `uvm_do_with(req, {req.AWADDR == prev_addr; req.AWVALID == 1; req.WSTRB == 4'b1111; req.WVALID == 1; req.WDATA != prev_data; req.BREADY == 1;})
+      prev_data = req.WDATA;
+
+      `uvm_do_with(req, {req.ARADDR == prev_addr; req.ARVALID == 1; req.RREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Write with strobe select and read back from the same address
+class write_strobe_select_1 extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(write_strobe_select_1)
+  int prev_addr;
+
+  function new(string name = "write_strobe_select_1");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Write with strobe select and read back from the same address  ------ ", UVM_NONE)
+    repeat(25) begin
+      req = axi4_seq_item::type_id::create("req");
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WVALID == 1; req.AWSTRB inside {[0:15]}; req.BREADY == 1;})
+      `uvm_do_with(req, {req.ARADDR == prev_addr; req.ARVALID == 1; req.RREADY == 1;})
+    end
+  endtask
+endclass
+
+class write_strobe_select_2 extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(write_strobe_select_2)
+  int prev_addr;
+
+  function new(string name = "write_strobe_select_2");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Write with strobe select and read back from the same address  ------ ", UVM_NONE)
+    repeat(5) begin
+      req = axi4_seq_item::type_id::create("req");
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WVALID == 1; req.AWSTRB == 4'b0001; req.BREADY == 1;})
+      prev_addr = req.AWADDR;
+      `uvm_do_with(req, {req.ARADDR == prev_addr; req.ARVALID == 1; req.RREADY == 1;})
+
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WVALID == 1; req.AWSTRB == 4'b0010; req.BREADY == 1;})
+      prev_addr = req.AWADDR;
+      `uvm_do_with(req, {req.ARADDR == prev_addr; req.ARVALID == 1; req.RREADY == 1;})
+
+      `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WVALID == 1; req.AWSTRB == 4'b0100; req.BREADY == 1;})
+      prev_addr = req.AWADDR;
+      `uvm_do_with(req, {req.ARADDR == prev_addr; req.ARVALID == 1; req.RREADY == 1;})
+      
+        `uvm_do_with(req, {req.AWADDR inside {0,4,8}; req.AWVALID == 1; req.WVALID == 1; req.AWSTRB == 4'b1000; req.BREADY == 1;})
+      prev_addr = req.AWADDR;
+      `uvm_do_with(req, {req.ARADDR == prev_addr; req.ARVALID == 1; req.RREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Invalid Address Write and Read
+class invalid_addr extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(invalid_addr)
+
+  function new(string name = "invalid_addr");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Invalid Address Write and Read ------ ", UVM_NONE)
+    repeat(10) begin
+      req = axi4_seq_item::type_id::create("req");
+      `uvm_do_with(req, {!(req.ARADDR inside {0,4,8}); req.ARVALID == 1; req.RREADY == 1; req.AWADDR inside {0,4,8}); req.AWVALID == 1; req.WVALID == 1; req.AWSTRB == 4'b1111; req.BREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Interrupt High
+class irq_seq_1 extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(irq_seq_1)
+
+  function new(string name = "irq_seq_1");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Interrupt High ------ ", UVM_NONE)
+    repeat(3) begin
+      `uvm_do_with(req, {req.AWADDR == 8; req.AWVALID == 1; req.WSTRB[0] == 1; req.WDATA[0] == 1; req.WVALID == 1; req.BREADY == 1; req.ext_irq_in == 1;})
+      #1000000;
+      `uvm_do_with(req, {req.ARADDR == 8; req.ARVALID == 1; req.RREADY == 1;})
+    end
+  endtask
+endclass
+
+// -----------------------------------------------------------------
+// Interrupt 
+/*
+class irq_seq_2 extends uvm_sequence#(axi4_seq_item);
+  `uvm_object_utils(irq_seq_2)
+
+  function new(string name = "irq_seq_2");
+    super.new(name);
+  endfunction
+
+  task body();
+    `uvm_info(get_type_name(), " ------ Interrupt High ------ ", UVM_NONE)
+    repeat(4) begin
+      `uvm_do_with(req, {req.AWADDR == 8; req.AWVALID == 1; req.WSTRB[0] == 1; req.WDATA[0] == 1; req.WVALID == 1; req.BREADY == 1; req.ext_irq_in == 1;})
+      #10_000;
+      `uvm_do_with(req, {req.AWADDR == 8; req.AWVALID == 1; req.WSTRB[0] == 1; req.WDATA[0] == 1; req.WVALID == 1; req.BREADY == 1; req.ext_irq_in == 0;})
+    end
+  endtask
+endclass
+*/
+// -----------------------------------------------------------------
+
