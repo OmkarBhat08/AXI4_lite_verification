bbd458727443809610aaae13c01acd49fb5f6ded
pratikshaa-07 <pratikshashetty125@gmail.com>
2026-02-25 17:25:27 +0530

    Update axi4_environment.sv

--- design_2_prithviraj/src/axi4_environment.sv
+++ design_2_prithviraj/src/axi4_environment.sv
@@ -3,7 +3,7 @@ class axi4_environment extends uvm_env;
   `uvm_component_utils(axi4_environment)
 
   axi4_active_agent   act_agent;
-  axi4_passive_agent  pas_agent;
+  //axi4_passive_agent  pas_agent;
   axi4_scoreboard     scb;
   axi4_subscriber     cov;
 
@@ -17,7 +17,7 @@ class axi4_environment extends uvm_env;
     super.build_phase(phase);
 
     act_agent = axi4_active_agent::type_id::create("act_agent", this);
-    pas_agent = axi4_passive_agent::type_id::create("pas_agent", this);
+    //pas_agent = axi4_passive_agent::type_id::create("pas_agent", this);
     scb       = axi4_scoreboard::type_id::create("scb", this);
     cov       = axi4_subscriber::type_id::create("cov", this);
 
@@ -28,12 +28,12 @@ class axi4_environment extends uvm_env;
     super.connect_phase(phase);
 
     // monitors to scoreboard fifos
-    act_agent.a_mon_h.a_mon_port.connect(scb.active_fifo.analysis_export);
-    pas_agent.p_mon_h.p_mon_port.connect(scb.passive_fifo.analysis_export);
+    act_agent.a_mon_h.a_mon_port.connect(scb.monitor_fifo.analysis_export);
+    //pas_agent.p_mon_h.p_mon_port.connect(scb.passive_fifo.analysis_export);
 
     // monitors to subscriber fifos
-    act_agent.a_mon_h.a_mon_port.connect(cov.inp_fifo.analysis_export);
-    pas_agent.p_mon_h.p_mon_port.connect(cov.op_fifo.analysis_export);
+    act_agent.a_mon_h.a_mon_port.connect(cov.monitor_fifo.analysis_export);
+    //pas_agent.p_mon_h.p_mon_port.connect(cov.op_fifo.analysis_export);
 
   endfunction 
 
