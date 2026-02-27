8c714ca70922ec277534dc9db2d6201bc0dcb244
NISHANTH GOWDA CJ <nishanthjgowda9740@gmail.com>
2026-02-27 12:43:38 +0530

    Refactor axi4_environment to use axi4_agent

--- design_1_shreenidhi/src/axi4_environment.sv
+++ design_1_shreenidhi/src/axi4_environment.sv
@@ -2,41 +2,36 @@ class axi4_environment extends uvm_env;
 
   `uvm_component_utils(axi4_environment)
 
-  axi4_active_agent   act_agent;
-  axi4_passive_agent  pas_agent;
-  axi4_scoreboard     scb;
-  axi4_subscriber     cov;
+  axi4_agent       agent_h;
+  axi4_scoreboard  scb;
+  axi4_subscriber  cov;
 
-
-  function new(string name = "axi4_environment",uvm_component parent = null);
+  function new(string name = "axi4_environment",
+               uvm_component parent = null);
     super.new(name, parent);
-  endfunction 
+  endfunction
+
 
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
 
-    act_agent = axi4_active_agent::type_id::create("act_agent", this);
-    pas_agent = axi4_passive_agent::type_id::create("pas_agent", this);
-    scb       = axi4_scoreboard::type_id::create("scb", this);
-    cov       = axi4_subscriber::type_id::create("cov", this);
-
-  endfunction 
+    agent_h = axi4_agent::type_id::create("agent_h", this);
+    scb     = axi4_scoreboard::type_id::create("scb", this);
+    cov     = axi4_subscriber::type_id::create("cov", this);
+  endfunction
 
 
+  //----------------------------------------
   function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
 
-    // monitors to scoreboard fifos
-    act_agent.a_mon_h.a_mon_port.connect(scb.active_fifo.analysis_export);
-    pas_agent.p_mon_h.p_mon_port.connect(scb.passive_fifo.analysis_export);
+    // Monitor - Scoreboard
+    agent_h.mon_h.mon_port.connect(scb.mon_fifo.analysis_export);
 
-    // monitors to suscriber fifos
-    act_agent.a_mon_h.a_mon_port.connect(cov.inp_fifo.analysis_export);
-    pas_agent.p_mon_h.p_mon_port.connect(cov.op_fifo.analysis_export);
+    // Monitor - Coverage
+    agent_h.mon_h.mon_port.connect(cov.inp_fifo.analysis_export);
+    agent_h.mon_h.mon_port.connect(cov.op_fifo.analysis_export);
+  endfunction
 
-  endfunction 
-
-
-endclass 
-
+endclass
