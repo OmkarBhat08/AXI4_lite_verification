f0abb57e2b55cbc510cc3abf7a87be0f160fd284
NISHANTH GOWDA CJ <nishanthjgowda9740@gmail.com>
2026-02-27 12:53:10 +0530

    Update axi4_sequencer.sv

--- design_1_shreenidhi/src/axi4_sequencer.sv
+++ design_1_shreenidhi/src/axi4_sequencer.sv
@@ -1,8 +1,10 @@
-class axi4_sequencer extends uvm_sequencer#(axi4_seq_item);
+class axi4_sequencer extends uvm_sequencer #(axi4_seq_item);
 
-  function new(string name="",uvm_component parent);
+  `uvm_component_utils(axi4_sequencer)
+
+  function new(string name="axi4_sequencer",
+               uvm_component parent);
     super.new(name,parent);
   endfunction
 
 endclass
-
