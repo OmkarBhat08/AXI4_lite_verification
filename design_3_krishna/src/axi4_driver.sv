a9ce69b6ecc2b698683bb8287692170c97b835c1
Alex Varghese <117454288+Alex-Varghese@users.noreply.github.com>
2026-03-10 09:14:09 +0530

    added timeout to axi4_driver class and its methods

--- design_3_krishna/src/axi4_driver.sv
+++ design_3_krishna/src/axi4_driver.sv
@@ -1,3 +1,4 @@
+/*
 class axi4_driver extends uvm_driver #(axi4_seq_item);
 
   `uvm_component_utils(axi4_driver)
@@ -188,4 +189,228 @@ class axi4_driver extends uvm_driver #(axi4_seq_item);
   endtask
 
 endclass
+*/
 
+class axi4_driver extends uvm_driver #(axi4_seq_item);
+
+  `uvm_component_utils(axi4_driver)
+
+  virtual axi4_if.DRV vif;
+
+  static int i;
+  static bit rd_addr_done;
+  static bit wrt_data_done, wrt_addr_done;
+  
+  int timeout = 500; 
+
+  function new(string name = "axi4_driver", uvm_component parent = null);
+    super.new(name, parent);
+
+    i = 0;
+    rd_addr_done   = 0;
+    wrt_data_done  = 0;
+    wrt_addr_done  = 0;
+
+  endfunction
+
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+
+    if (!(uvm_config_db #(virtual axi4_if)::get(this, "", "vif", vif)))
+    begin
+      `uvm_fatal("DRIVER", "NO VIRTUAL INTERFACE IN DRIVER")
+    end
+
+  endfunction
+
+  task run_phase(uvm_phase phase);
+
+    repeat (3) @(vif.drv_cb);
+
+    forever
+      begin
+        seq_item_port.get_next_item(req);
+        drive();
+        seq_item_port.item_done();
+        i++;
+    end
+
+  endtask
+
+  task drive();
+
+    vif.drv_cb.AWADDR<=req.AWADDR;
+    vif.drv_cb.AWVALID<=req.AWVALID;
+
+    vif.drv_cb.WDATA<=req.WDATA;
+    vif.drv_cb.WSTRB<=req.WSTRB;
+    vif.drv_cb.WVALID<=req.WVALID;
+
+    vif.drv_cb.BREADY<=req.BREADY;
+
+    vif.drv_cb.ARADDR<=req.ARADDR;
+    vif.drv_cb.ARVALID<=req.ARVALID;
+
+    vif.drv_cb.RREADY<=req.RREADY;
+
+    vif.drv_cb.EXT_IRQ_IN<=req.EXT_IRQ_IN;
+
+    `uvm_info("DRV",$sformatf("------------------------------DRIVER - %0d Driving-----------------------",i),UVM_LOW)
+
+    `uvm_info("DRV",$sformatf("Write address channel : AWADDR = 0x%0h | AWVALID = %0b ",req.AWADDR, req.AWVALID),UVM_LOW)
+    `uvm_info("DRV",$sformatf("Write data channel : WDATA = 0x%0h | WSTRB = 0x%0d | WVALID = %0b",req.WDATA, req.WSTRB, req.WVALID),UVM_LOW)
+    `uvm_info("DRV",$sformatf("Write response channel : BREADY = %0b", req.BREADY),UVM_LOW)
+    `uvm_info("DRV",$sformatf("Read address channel :ARADDR = 0x%0h, ARVALID = %0b",req.ARADDR, req.ARVALID),UVM_LOW)
+    `uvm_info("DRV",$sformatf("Read data channel : RREADY = %0b", req.RREADY),UVM_LOW)
+    `uvm_info("DRV",$sformatf("Extra irq input : EXT_IRQ_IN = %0b", req.EXT_IRQ_IN),UVM_LOW)
+
+    `uvm_info("DRV",$sformatf("-------------------------------------------------------------------------------------"),UVM_LOW)
+
+    fork
+      check_wrt_addr();
+      check_wrt_data();
+      check_wrt_resp();
+      check_rd_addr();
+    join
+
+    @(vif.drv_cb);
+
+  endtask
+
+  task check_rd_addr();
+    if (rd_addr_done == 0)
+    begin
+      if(req.ARVALID == 1)
+      begin
+        
+        fork : arready_wait
+          begin
+            wait(vif.drv_cb.ARREADY); 
+          end
+          begin
+            repeat(timeout) @(vif.drv_cb); 
+            `uvm_fatal("TIMEOUT", "Deadlock: Slave never asserted ARREADY")
+          end
+        join_any
+        disable arready_wait; 
+        
+        rd_addr_done=1;
+        @(vif.drv_cb);
+        vif.drv_cb.ARVALID<=0;
+        check_rd_data();
+      end
+    end
+    else
+    begin
+      check_rd_data();
+    end
+  endtask
+
+  task check_rd_data();
+    if(req.RREADY == 1)
+    begin
+      
+      fork : rvalid_wait
+        begin
+          wait(vif.drv_cb.RVALID);
+        end
+        begin
+          repeat(timeout) @(vif.drv_cb);
+          `uvm_fatal("TIMEOUT", "Deadlock: Slave never asserted RVALID")
+        end
+      join_any
+      disable rvalid_wait;
+      
+      rd_addr_done=0;
+      @(vif.drv_cb);
+      vif.drv_cb.RREADY<=0;
+    end
+  endtask
+
+  task check_wrt_addr();
+    if(wrt_addr_done==0)
+    begin
+      if(req.AWVALID==1)
+      begin
+        
+        fork : awready_wait
+          begin
+            wait(vif.drv_cb.AWREADY);
+          end
+          begin
+            repeat(timeout) @(vif.drv_cb);
+            `uvm_fatal("TIMEOUT", "Deadlock: Slave never asserted AWREADY")
+          end
+        join_any
+        disable awready_wait;
+        
+        wrt_addr_done=1;
+
+        fork
+          if(wrt_data_done && wrt_addr_done)
+            check_wrt_resp();
+          @(vif.drv_cb)
+          begin
+            vif.drv_cb.AWVALID<=0;
+          end
+        join
+
+      end
+    end
+  endtask
+
+  task check_wrt_data();
+    if(wrt_data_done==0)
+    begin
+      if(req.WVALID==1)
+      begin
+        
+        fork : wready_wait
+          begin
+            wait(vif.drv_cb.WREADY);
+          end
+          begin
+            repeat(timeout) @(vif.drv_cb);
+            `uvm_fatal("TIMEOUT", "Deadlock: Slave never asserted WREADY")
+          end
+        join_any
+        disable wready_wait;
+        
+        wrt_data_done=1;
+
+        fork
+          if(wrt_data_done && wrt_addr_done)
+            check_wrt_resp();
+
+          @(vif.drv_cb)
+          begin
+            vif.drv_cb.WVALID<=0;
+          end
+        join
+
+      end
+    end
+  endtask
+
+  task check_wrt_resp();
+    if(req.BREADY==1)
+    begin
+      
+      fork : bvalid_wait
+        begin
+          wait(vif.drv_cb.BVALID);
+        end
+        begin
+          repeat(timeout) @(vif.drv_cb);
+          `uvm_fatal("TIMEOUT", "Deadlock: Slave never asserted BVALID")
+        end
+      join_any
+      disable bvalid_wait;
+      
+      wrt_data_done=0;
+      wrt_addr_done=0;
+      vif.drv_cb.BREADY<=0;
+    end
+  endtask
+
+endclass
